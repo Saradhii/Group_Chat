20da57227c81b266967e5ee3ae70c82f6d412950
Saradhi <99667252+Saradhii@users.noreply.github.com>
2022-08-21 18:10:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+const {Server} = require("socket.io");
+
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
+const history=[];
+
+io.on("connection",(socket)=>{
+    socket.emit("history",history);
+    socket.broadcast.emit("newUser");
+    socket.on("newMessage",(msg)=>{
+       history.push(msg);
+       io.emit("newMessage",msg);
+    });
+
+});
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use("/",express.static("public"));
+
+// app.get("/",(req,res)=> res.sendFile(__dirname + "/public/index.html" ));
+
+const port = process.env.PORT || 9000;
+
+httpServer.listen(port,()=>{
+    console.log("started on http://localhost:9000")
+});
